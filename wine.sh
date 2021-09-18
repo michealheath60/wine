185228e60632963ca765cd94959ef70c64377ec9
michealheath60 <62250578+michealheath60@users.noreply.github.com>
2021-09-18 16:04:12 +0530

    Update wine.sh

--- wine.sh
+++ wine.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y install software-properties-common
 wget -qO - https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
 apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-add-apt-repository ppa:cybermax-dexter/sdl2-backport
+yes | add-apt-repository ppa:cybermax-dexter/sdl2-backport
 apt update
 wget -O- -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/Release.key | apt-key add -
 echo "deb http://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04 ./" | tee /etc/apt/sources.list.d/wine-obs.list
